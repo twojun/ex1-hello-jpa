6cdfa58e4accb643eeee794af57e6137d8e1bcd3
yoowonjun <twojunspace@naver.com>
2024-01-08 22:21:41 +0900

    Basic mapping

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,35 +4,23 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
 
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-
-        // transaction start : jpa 내부 동작은 트랜잭션 범위 내에서 발생되어야 한다.
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("c");
-            System.out.println("member.getId() = " + member.getId());
-
-            System.out.println("===========");
-            em.persist(member);
-            System.out.println("member.getId() = " + member.getId());
-            System.out.println("============");
-
             tx.commit();
 
         } catch (Exception e) {
             tx.rollback();
         } finally {
             em.close();
-            emf.close();
         }
+        emf.close();
     }
 }
